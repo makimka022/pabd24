331cb59ffda7fb3a73c8339f8de3ded30fe33276
makimka022 <8937419@gmail.com>
2024-05-20 19:28:25 +0300

    load train/val sets

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -22,9 +22,9 @@ TRAIN_SIZE = 0.8
 
 
 def main(args):
-    main_dataframe = pd.read_csv(args.input[0], delimiter=';')
+    main_dataframe = pd.read_csv(args.input[0], delimiter=',')
     for i in range(1, len(args.input)):
-        data = pd.read_csv(args.input[i], delimiter=';')
+        data = pd.read_csv(args.input[i], delimiter=',')
         df = pd.DataFrame(data)
         main_dataframe = pd.concat([main_dataframe, df], axis=0)
 
