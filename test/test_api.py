9039f94b58d84a62795c3d717f4bc6ac62252809
makimka022 <8937419@gmail.com>
2024-05-27 20:11:37 +0300

    add utils

--- test/test_api.py
+++ test/test_api.py
@@ -1,22 +1,29 @@
 import unittest
 import requests
 from dotenv import dotenv_values
+import time
 
 config = dotenv_values(".env")
-ENDPOINT = 'http://127.0.0.1:5000'
+ENDPOINT = 'http://192.144.14.8:5000'
 HEADERS = {"Authorization": f"Bearer {config['APP_TOKEN']}"}
 
 
 class TestApi(unittest.TestCase):
     def test_home(self):
+        t0 = time.time()
         resp = requests.get(ENDPOINT)
         self.assertIn('Housing price service', resp.text)
+        t1 = time.time()
+        print(f'test home: {t1-t0}')
 
     def test_api(self):
+        t0 = time.time()
         data = {'area': 42}
         resp = requests.post(ENDPOINT +'/predict',
                              json=data,
                              headers=HEADERS)
+        t1 = time.time()
+        print(f'test predict: {t1-t0}')
         self.assertIn('price', resp.text)
 
 
